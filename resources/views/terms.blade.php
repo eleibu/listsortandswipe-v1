6e1cd8298f56afc653a4986aa6bebbd49bf2662d
eleibu <elliot.leibu@gmail.com>
2019-03-15 13:41:19 +1100

    Terms done

--- resources/views/terms.blade.php
+++ resources/views/terms.blade.php
@@ -19,10 +19,10 @@
 <div class="section-cont">
     <div class="section-outer">
         <div class="para">
-            This agreement (the <strong>Agreement</strong>) is made between Indysoft Pty Ltd ACN 612 045 002 (a company incorporated in Australia) (<strong>Indysoft</strong>) and the Licensee (the <strong>Licensee</strong>). By downloading, installing, copying, accessing and/or using Lithium List, the Licensee agrees to be bound by the terms and conditions of this Agreement.
+            This agreement is made between Indysoft and the Licensee. By downloading, installing, copying, accessing and/or using Lithium List, the Licensee agrees to be bound by the terms and conditions of this agreement.
         </div>
         <div class="para">
-            Indysoft may alter this Agreement at any time. The altered version, which immediately becomes the Licensee&#39;s agreement with Indysoft, will be the version published on the Website.
+            Indysoft may alter this agreement at any time. The altered version, which immediately becomes the Licensee&#39;s agreement with Indysoft, will be the version published on the Website.
         </div>
     </div>
 </div>
@@ -32,15 +32,24 @@
             1. Definitions
         </div>
         <div class="para">
-            <strong>License</strong> means the license granted by Indysoft on the terms of this Agreement.
+            In this agreement the following terms have these meanings:
         </div>
-        <div class="para">
+        <div class="para def">
+            <strong>Indysoft</strong> means Indysoft Pty Ltd ACN 612 045 002, a company incorporated in Australia.
+        </div>
+        <div class="para def">
+            <strong>License</strong> means the license granted by Indysoft on the terms of this agreement.
+        </div>
+        <div class="para def">
+            <strong>Licensee</strong> means the individual or entity agreeing to the terms and conditions of this agreement.
+        </div>
+        <div class="para def">
             <strong>Lithium List</strong> means the sortable and swipeable list javascript plugin owned by Indysoft and licensed to third parties.
         </div>
-        <div class="para">
+        <div class="para def">
             <strong>Price List</strong> means the pricing for the relevant licence type, as set out on the Website at the time of purchase.
         </div>
-        <div class="para">
+        <div class="para def">
             <strong>Website</strong> means all web pages and related material available via the domain name lithiumlist.com.
         </div>
     </div>
@@ -52,16 +61,328 @@
         </div>
         <div class="para">
             The Licensee ackowledges that:
-        </div>
-        <div class="para">
-            <ol type="a">
+            <ol type="i">
                 <li>
-                    Indysoft owns all intellectual property rights in and over Lithium List. Nothing in this Agreement operates to assign or transfer any intellectual property rights from Indysoft to you.
+                    Indysoft owns all intellectual property rights in and over Lithium List. Nothing in this agreement operates to assign or transfer any intellectual property rights from Indysoft to you.
                 </li>
                 <li>
                     The structure, organisation and code of Lithium List are valuable to Indysoft. The Lithium List logo and, if applicable, trademarks are proprietary to Indysoft and protected by law.
                 </li>
             </ol>
+        </div>
+    </div>
+</div>
+<div class="section-cont grey">
+    <div class="section-outer">
+        <div class="title">
+            3. Grant of License
+        </div>
+        <div class="para">
+            <ol type="a">
+                <li>
+                    Lithium List is licensed in accordance with these terms.
+                </li>
+                <li>
+                    The Licence is:
+                    <ol type="i">
+                        <li>
+                            non-exclusive, non-transferable, non-sublicensable and non-reusable;
+                        </li>
+                        <li>
+                            subject to restrictions, if any, specified on the Website; and
+                        </li>
+                        <li>
+                            granted at the time, and for the term, specified here:
+                            <table cellspacing="0" cellpadding="0">
+                                <tbody>
+                                    <tr>
+                                        <td class="heading">Licence type</td>
+                                        <td class="heading">When granted</td>
+                                        <td class="heading">Term</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Free trial</td>
+                                        <td>When the Licensee first creates a Lithium List user account</td>
+                                        <td>30 days</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Other</td>
+                                        <td>When payment is received by Indysoft</td>
+                                        <td>1 year</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </li>
+                    </ol>
+                </li>
+                <li>
+                    The Licensee may copy Lithium List only:
+                    <ol type="i">
+                        <li>
+                            to the extent reasonable necessary to use Lithium List in accordance with the Licence terms; and
+                        </li>
+                        <li>
+                            if each copy contains all proprietary notices contained in the original version provided by Indysoft.
+                        </li>
+                    </ol>
+                </li>
+                <li>
+                    Subject to b. and c. above, the Licensee may not transfer, rent, lease, lend, sell, copy, redistribute or sublicense Lithium List.
+                </li>
+                <li>
+                    Indysoft reserves all rights not expressly granted to the Licensee in this agreement.
+                </li>
+            </ol>
+        </div>
+    </div>
+</div>
+<div class="section-cont">
+    <div class="section-outer">
+        <div class="title">
+            4. General restrictions
+        </div>
+        <div class="para">
+            The Licensee agrees that it will not itself, or through any parent, subsidiary, affiliate, agent or other third party:
+            <ol type="i">
+                <li>
+                    sell Lithium List, with or without modification;
+                </li>
+                <li>
+                    remove the unlicensed banner from Lithium List;
+                </li>
+                <li>
+                    remove any copyright notice, or other notice, included in the source code of Lithium List;
+                </li>
+                <li>
+                    use Lithium List in any open source software or projects;
+                </li>
+                <li>
+                    use the License in more than the permitted number of websites;
+                </li>
+                <li>
+                    reverse engineer, decompile or disassemble the source code of Lithium List;
+                </li>
+                <li>
+                    use Lithium List for any illegal activity, or in any website or project that may diminish the value of the Lithium List brand;
+                </li>
+                <li>
+                    use Lithium List in a product, service or software that is, directly or indirectly, competitive with or in any way a substitute for Lithium List;
+                </li>
+                <li>
+                    take any action that might diminish or cancel any of Indysoft’s rights over or in respect of Lithium List; or
+                </li>
+                <li>
+                    allow and/or request any third party to take any action described above.
+                </li>
+            </ol>
+        </div>
+    </div>
+</div>
+<div class="section-cont grey">
+    <div class="section-outer">
+        <div class="title">
+            5. Licence types
+        </div>
+        <div class="para">
+            The licence types, including restrictions applicable to each licence type, are set out in:
+            <ol type="i">
+                <li>
+                    the Website, including the pricing page; and
+                </li>
+                <li>
+                    this agreement.
+                </li>
+            </ol>
+        </div>
+    </div>
+</div>
+<div class="section-cont">
+    <div class="section-outer">
+        <div class="title">
+            6. Licence fee
+        </div>
+        <div class="para">
+            The licence fee is calculated at the rate set out in the Price List. The fees listed in the Price List do not include taxes. If Indysoft is required to pay any sales, use, property, excise, value added, gross receipts, withholding or other taxes levied on Lithium List or on the Licensee&#39;s use thereof, such taxes will be billed to and paid by the Licensee. Such taxes do not include taxes applicable to Indysoft’s net income or employer contributions.
+        </div>
+    </div>
+</div>
+<div class="section-cont grey">
+    <div class="section-outer">
+        <div class="title">
+            7. Licence keys
+        </div>
+        <div class="para">
+            <ol type="a">
+                <li>
+                    Upon obtaining a Licence, the Licensee may obtain from the Website console page one of the following (depending on the Licensee&#39;s account type):
+                    <ol type="i">
+                        <li>
+                            one licence key per domain; or
+                        </li>
+                        <li>
+                            a licence key that you may use with all domains.
+                        </li>
+                    </ol>
+                </li>
+                <li>
+                    Licence keys:
+                    <ol type="i">
+                        <li>
+                            may not be disclosed, and are not transferable, to any other person; and
+                        </li>
+                        <li>
+                            are not transferable between domains.
+                        </li>
+                    </ol>
+                </li>
+            </ol>
+        </div>
+    </div>
+</div>
+<div class="section-cont">
+    <div class="section-outer">
+        <div class="title">
+            8. Refunds
+        </div>
+        <div class="para">
+            <ol type="a">
+                <li>
+                    Subject to applicable laws, the licence fee is not refundable.
+                </li>
+                <li>
+                    Indysoft may nevertheless permit refunds on such conditions as it determines. If Indysoft permits and provides a refund, the Licensee&#39;s rights under this agreement terminate immediately.
+                </li>
+            </ol>
+        </div>
+    </div>
+</div>
+<div class="section-cont grey">
+    <div class="section-outer">
+        <div class="title">
+            9. Representations and warranties
+        </div>
+        <div class="para">
+            <ol type="a">
+                <li>
+                    The Licensee represents and warrants that:
+                    <ol type="i">
+                        <li>
+                            all information provided to Indysoft by or on behalf of the Licensee is true, correct and complete; and
+                        </li>
+                        <li>
+                            it, he or she has the legal right to use any credit card(s) or other payment method(s) in connection with obtaining a Licence or otherwise make purchases from Indysoft.
+                        </li>
+                    </ol>
+                </li>
+                <li>
+                    If the Licensee is not an individual, each individual who interacts with Indysoft and/or the Website represents and warrants that they have the power and authority to:
+                    <ol type="i">
+                        <li>
+                            act on behalf of the Licensee in relation to all such interactions;
+                        </li>
+                        <li>
+                            provide all relevant information regarding the Licensee to Indysoft; and
+                        </li>
+                        <li>
+                            agree, on the Licensee’s behalf, to this agreement.
+                        </li>
+                    </ol>
+                </li>
+            </ol>
+        </div>
+    </div>
+</div>
+<div class="section-cont">
+    <div class="section-outer">
+        <div class="title">
+            10. Termination
+        </div>
+        <div class="para">
+            <ol type="a">
+                <li>
+                    All terms of this agreement are material to Indysoft. If you breach a term, Indysoft may terminate your rights under this agreement immediately by giving you notice in writing or by email.
+                </li>
+                <li>
+                    Upon termination of your rights under this agreement, whether pursuant to subparagraph 1. or otherwise:
+                    <ol type="i">
+                        <li>
+                            the Licensee must immediately remove, delete or otherwise destroy any material that it has received in connection with the Licence, and must procure that all copies of any such material are removed, deleted or otherwise destroyed; and
+                        </li>
+                        <li>
+                            this agreement otherwise remains in full force and effect.
+                        </li>
+                    </ol>
+                </li>
+            </ol>
+        </div>
+    </div>
+</div>
+<div class="section-cont grey">
+    <div class="section-outer">
+        <div class="title">
+            11. No warranty
+        </div>
+        <div class="para">
+            Lithium List is provided by Indysoft on an &#39;as is&#39; basis. Any express or implied warranty is excluded to the maximum extent permitted by law. Without limiting the foregoing, Indysoft makes no representation or warranty that:
+            <ol type="i">
+                <li>
+                    Lithium List will meet the Licensee’s requirements;
+                </li>
+                <li>
+                    Lithium List is bug free or otherwise of merchantable quality; or
+                </li>
+                <li>
+                    Lithium List and/or the Website will be available, updated, secure or error free.
+                </li>
+            </ol>
+        </div>
+    </div>
+</div>
+<div class="section-cont">
+    <div class="section-outer">
+        <div class="title">
+            12. Limitation of liability
+        </div>
+        <div class="para">
+            Indysoft&#39;s maximum liability, whether in contract, tort or otherwise, to the Licensee in relation to Lithium List is equal to the total licence fees paid by the Licensee to Indysoft. Indysoft has no liability for any indirect losses, costs or damages, including lost revenue, profits or brand value. This limit applies across all claims brought by or on behalf of the Licensee.
+        </div>
+    </div>
+</div>
+<div class="section-cont grey">
+    <div class="section-outer">
+        <div class="title">
+            13. Notices
+        </div>
+        <div class="para">
+            All communications in relation to this agreement must be given by email and addressed to:
+            <ol type="i">
+                <li>
+                    for communications to Indysoft: <a href="mailto:admin@lithiumlist.com" title="admin@lithiumlist.com">admin@lithiumlist.com</a>; and
+                </li>
+                <li>
+                    for communications to the Licensee: the email address supplied by or on behalf of the Licensee when its user account was created or, if applicable, any replacement email address supplied by or on behalf of the Licensee via the Website.
+                </li>
+            </ol>
+        </div>
+    </div>
+</div>
+<div class="section-cont">
+    <div class="section-outer">
+        <div class="title">
+            14. Assignment and third parties
+        </div>
+        <div class="para">
+            The Licensee may not assign its rights, or transfer its obligations, under this agreement without Indysoft’s prior written consent. No third party has any rights under this agreement.
+        </div>
+    </div>
+</div>
+<div class="section-cont grey">
+    <div class="section-outer">
+        <div class="title">
+            15. Governing law
+        </div>
+        <div class="para">
+            This agreement is governed by the laws in Victoria, Australia. The parties submit to the non-exclusive jurisdiction of the courts of that place.
         </div>
     </div>
 </div>
